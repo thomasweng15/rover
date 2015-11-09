2d0f8cb058aaeb34239604c374e8c9650c8a6c4f
thomasweng15 <thomas.weng11@gmail.com>
2015-11-09 07:13:48 +0000

    Initial work on rover controller class

--- src/rover.py
+++ src/rover.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python
 
-print "hello world"
+import json
+import rospy
+from std_msgs.msg import String
+
+class RoverCtrl:
+	def __init__(self, threshold):
+		rospy.init_node("rover_controller")
+		self.sub = rospy.Subscriber("xboxdrv", String, self.xboxdrv_callback)
+		self.threshold = threshold
+		self.Y1 = 0
+		self.Y2 = 0
+
+	def xboxdrv_callback(self, data):
+		datadict = json.loads(data.data)
+		self.Y1 = int(datadict["Y1"])
+		self.Y2 = int(datadict["Y2"])
+		if abs(self.Y1) - self.threshold > 0:
+			print "left motor: " + str(self.Y1)
+		elif abs(self.Y2) - self.threshold > 0:
+			print "right motor: " + str(self.Y2)
+
+	def run(self):
+		rospy.spin()
+
+if __name__ == '__main__':
+	rover = RoverCtrl(10000)
+	rover.run()
