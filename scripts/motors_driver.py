110db8628e2fd88c83a595d2353b682fe8a805ba
thomasweng15 <thomas.weng11@gmail.com>
2018-06-07 13:53:52 -0400

    Add velocity to power equation for speed control

--- scripts/motors_driver.py
+++ scripts/motors_driver.py
@@ -47,6 +47,16 @@ class MotorsDriver:
         self.stop()
         GPIO.cleanup()
 
+    def velocity_to_power(self, v):
+        if v == 0:
+            return 0
+
+        min_v = 0
+        max_v = 0.35
+        clamped_v = max(min_v, min(abs(v), max_v))
+        power = 40.2 + 35*clamped_v + 405*(clamped_v**2)
+        return min(power, 100)
+
     def _telop_callback(self, msg):
         """
         Move forward at constant speed.
@@ -67,9 +77,10 @@ class MotorsDriver:
         self._pub_vel_l.publish(Float64(vel_l))
         self._pub_vel_r.publish(Float64(vel_r))
         
-        power = 0 if th == 0 and x == 0 else 100
-        self._left.update(power, vel_l >= 0)
-        self._right.update(power, vel_r >= 0)
+        power_l = self.velocity_to_power(vel_l)
+        power_r = self.velocity_to_power(vel_r)
+        self._left.update(power_l, vel_l >= 0)
+        self._right.update(power_r, vel_r >= 0)
 
     def stop(self):
         self._left.stop()
