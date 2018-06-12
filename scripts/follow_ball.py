77940af6a229ddc3887c08da3ac934b687e451f2
Thomas Weng <thomas.weng11@gmail.com>
2018-06-12 18:51:08 -0400

    Add ros spinner and subscriber to camera image

--- scripts/follow_ball.py
+++ scripts/follow_ball.py
@@ -1,8 +1,18 @@
 import rospy
+from sensor_msgs.msg import Image
 
 class FollowBall:
+    def __init__(self):
+        self.sub = rospy.Subscriber("camera/image", Image, self.cb)
+
+    def cb(self, msg):
+        pass
+
     def run(self):
-        rospy.spin()
+        r = rospy.rate(10)
+        while not rospy.is_shutdown():
+            rospy.spinOnce()
+            r.sleep()
 
 if __name__ == '__main__':
     fb = FollowBall()
