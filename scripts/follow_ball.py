959aecfb21cba4235a7dfbc039dfd1485a80f483
Thomas Weng <thomas.weng11@gmail.com>
2018-06-12 18:55:19 -0400

    Add publish_cmd_vel function and publisher

--- scripts/follow_ball.py
+++ scripts/follow_ball.py
@@ -1,17 +1,23 @@
 import rospy
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
 
 class FollowBall:
     def __init__(self):
         self.sub = rospy.Subscriber("camera/image", Image, self.cb)
+        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
     def cb(self, msg):
+        pass
+
+    def publish_cmd_vel(self):
         pass
 
     def run(self):
         r = rospy.rate(10)
         while not rospy.is_shutdown():
             rospy.spinOnce()
+            self.publish_cmd_vel()
             r.sleep()
 
 if __name__ == '__main__':
