d5e9a1321a43a5d32ef76832b0364c7ebb007909
Thomas Weng <thomas.weng11@gmail.com>
2018-06-13 11:16:07 -0400

    Added MoveAPI

--- scripts/follow_ball.py
+++ scripts/follow_ball.py
@@ -4,15 +4,47 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
+class MoveAPI:
+    Idle, Forward, Rotate = range(3)
+
+    def __init__(self):
+        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.state = Idle
+
+    def idle(self):
+        if self.state == Idle:
+            return
+
+        msg = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+        self.pub.publish(msg)
+        self.state = Idle
+
+    def forward(self):
+        if self.state == Forward:
+            return
+
+        msg = Twist(Vector3(0.2, 0, 0), Vector3(0, 0, 0))
+        self.pub.publish(msg)
+        self.state == Forward
+
+    def rotate(self, clockwise=True):
+        if self.state == Rotate:
+            return
+
+        msg = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0.5))
+        self.pub.publish(msg)
+        self.state == Rotate
+
 class FollowBall:
     def __init__(self):
+        self.move = MoveApi()
         self.sub = rospy.Subscriber("camera/image", Image, self.cb)
-        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-        self.im_pub = rospy.Publisher("processed/image", Twist, queue_size=10)
+        self.pub = rospy.Publisher("processed/image", Twist, queue_size=10)
         self.bridge = CvBridge()
         self.im = np.zeros([410,308,3], dtype=np.uint8)
         self.greenLower = (29, 86, 6)
         self.greenUpper = (64, 255, 255)
+        self.circle = { center: None, radius: 0 }
 
     def cb(self, data):
         try: 
@@ -50,21 +82,33 @@ class FollowBall:
             # only proceed if radius meets a minimum size
             if radius > 10:
                 # draw circle and centroid on the frame
-                cv2.circle(self.im, (int(x), int(y)), int(radius)),
+                cv2.circle(self.im, (int(x), int(y)), int(radius),
                     (0, 255, 255), 2)
                 cv2.circle(self.im, center, 5, (0, 0, 255), -1)
 
+        self.circle.center = center
+        self.circle.radius = radius
+
     def publish_image(self):
         try:
-            self.im_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            self.pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         except CvBridgeError as e:
             print(e)
 
-    def compute_action(self):
-        pass
-
     def publish_action(self):
-        pass
+        if self.circle.center is None:
+            self.move.idle()
+            return
+        
+        if self.circle.radius > 150:
+            # stop
+            return 
+
+        if abs(self.circle.center[0] - 200) < 50:
+            # drive toward 
+            return 
+
+        # otherwise rotate so ball is close to center
 
     def run(self):
         r = rospy.rate(10)
@@ -72,7 +116,6 @@ class FollowBall:
             rospy.spinOnce()
             self.process_image()
             self.publish_image()
-            self.compute_action()
             self.publish_action()
             r.sleep()
 
