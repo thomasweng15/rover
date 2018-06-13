c7b6b44ed582c909707a8d63d69255fdce5b4814
Thomas Weng <thomas.weng11@gmail.com>
2018-06-12 20:41:55 -0400

    Added circle detection

--- scripts/follow_ball.py
+++ scripts/follow_ball.py
@@ -10,6 +10,8 @@ class FollowBall:
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
         self.bridge = CvBridge()
         self.im = np.zeros([410,308,3], dtype=np.uint8)
+        self.greenLower = (29, 86, 6)
+        self.greenUpper = (64, 255, 255)
 
     def cb(self, data):
         try: 
@@ -18,7 +20,38 @@ class FollowBall:
             print(e)
 
     def process_image(self):
-        pass
+        """https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
+        """
+        # blur frame and convert to HSV
+        blurred = cv2.GaussianBlur(self.im, (11, 11), 0)
+        hsv = cv2.cv2Color(blurred, cv2.COLOR_BGR2HSV)
+
+        # Construct mask for color green and apply to im
+        mask = cv2.inRange(hsv, self.greenLower, self.greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        center = None
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # Find largest contour and compute minimum closing circle and centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"]/M["m00"]))
+
+            # only proceed if radius meets a minimum size
+            if radius > 10:
+                # draw circle and centroid on the frame
+                cv2.circle(self.im, (int(x), int(y)), int(radius)),
+                    (0, 255, 255), 2)
+                cv2.circle(self.im, center, 5, (0, 0, 255), -1)
 
     def publish_action(self):
         pass
